8491b37e5e0694dab2b52f3b2234e0f49cef15fd
Bruno Soares <vsoares.bruno@gmail.com>
2024-01-18 19:55:19 +0000

    feat: create container

---
+++ src/components/TranscriptionContainer.vue
@@ -0,0 +1,25 @@
+<script>
+import TranscriptionsList from "./TranscriptionsList.vue";
+import TheHeader from "./TheHeader.vue";
+
+export default {
+  name: "TranscriptionContainer",
+  components: {
+    TheHeader,
+    TranscriptionsList,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  margin: 0 auto;
+  width: 740px;
+}
+</style>
+<template>
+  <TheHeader title="Trascriptions" />
+  <div class="container">
+    <TranscriptionsList />
+  </div>
+</template>
