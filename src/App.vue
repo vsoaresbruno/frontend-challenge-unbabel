6f7b01846bb59a526e0a5f3a5a0b34ea02b0c927
Bruno Soares <vsoares.bruno@gmail.com>
2024-01-18 19:56:19 +0000

    refactor: chnage structure

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,14 @@
-<template>
-  <HelloWorld msg="Welcome to Your Vue.js App" />
-</template>
-
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TranscriptionContainer from "./components/TranscriptionContainer.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    TranscriptionContainer,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <TranscriptionContainer />
+</template>
