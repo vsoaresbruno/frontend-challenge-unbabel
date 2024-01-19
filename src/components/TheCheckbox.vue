fa67794d4caf2114150c7e8516d11d682b35a731
Bruno Soares <vsoares.bruno@gmail.com>
2024-01-19 19:08:23 +0000

    feat: create checkbox

---
+++ src/components/TheCheckbox.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+  props: {
+    index: Number,
+  },
+};
+</script>
+<style scoped>
+.transcriptions__checkbox-wrapper,
+.transcriptions__face-icon-wrapper {
+  display: flex;
+  margin-right: 10px;
+}
+
+.transcriptions__checkbox {
+  opacity: 0;
+}
+
+.transcriptions__checkbox-label {
+  width: 12px;
+  height: 12px;
+  border: 2px solid var(--purple);
+  border-radius: 2px;
+  position: absolute;
+  margin-top: 5px;
+}
+
+.transcriptions__checkbox:checked + .transcriptions__checkbox-label {
+  background: var(--purple);
+}
+
+.transcriptions__checkbox:checked + .transcriptions__checkbox-label:after {
+  content: "";
+  position: absolute;
+  width: 8px;
+  height: 3px;
+  border-left: 2px solid #fff;
+  border-bottom: 2px solid #fff;
+  transform: rotate(-45deg);
+  top: 2px;
+  left: 1px;
+}
+</style>
+
+<template>
+  <div class="transcriptions__checkbox-wrapper">
+    <input
+      type="checkbox"
+      :id="`transcriptions__item-${index}`"
+      class="transcriptions__checkbox"
+    /><label
+      :for="`transcriptions__item-${index}`"
+      class="transcriptions__checkbox-label"
+    ></label>
+  </div>
+</template>
